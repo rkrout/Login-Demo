fe676a2b522ce7c1966210d14f51be47e2234e99
= <=>
2023-07-12 18:48:46 +0530

    modified

--- angular/ajax.php
+++ angular/ajax.php
@@ -1,54 +1,174 @@
-<?php 
+<?php
+
+error_reporting(1);
 
 $db = new PDO("mysql:host=localhost;dbname=students", "root", "");
 
-if($_GET["action"] == "get_students")
+switch ($_GET["action"]) 
 {
-    $stmt = $db->prepare("select * from students");
-    $stmt->execute();
-    $students = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($students);
-}
-else if($_GET["action"] == "create_student")
-{
-    error_reporting(1);
-    file_put_contents("demo.json", json_encode($_POST));
-    echo json_encode(["message" => "Student created successfully"]);
-die();
-    $image_path = "uploads/" . bin2hex(random_bytes(16)) . "." . pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
-    move_uploaded_file($_FILES["image"]["tmp_name"], $image_path);
-    $stmt = $db->prepare("insert into students (name, email, phone, image_url, active) values (:name, :email, :phone, :image_url, :active)");
-    $stmt->bindParam("name", $_POST["name"]);
-    $stmt->bindParam("email", $_POST["email"]);
-    $stmt->bindParam("phone", $_POST["phone"]);
-    $stmt->bindValue("active", $_POST["active"] == "true");
-    $stmt->bindParam("image_url", $image_path);
-    $stmt->execute();
-    echo json_encode(["message" => "Student created successfully"]);
-}
-else if($_GET["action"] == "get_student")
-{
-    file_put_contents("demo.json", json_encode($_POST));
-    $stmt = $db->prepare("select * from students where id = :id limit 1");
-    $stmt->bindParam("id", $_GET["id"]);
-    $stmt->execute();
-    echo json_encode($stmt->fetch(PDO::FETCH_ASSOC));
-}
-else if($_GET["action"] == "update_student")
-{
-    file_put_contents("demo.json", json_encode($_POST));
-    $stmt = $db->prepare("update students set name = :name, email = :email, phone = :phone where id = :id");
-    $stmt->bindParam("id", $_POST["id"]);
-    $stmt->bindParam("name", $_POST["name"]);
-    $stmt->bindParam("email", $_POST["email"]);
-    $stmt->bindParam("phone", $_POST["phone"]);
-    $stmt->execute();
-    echo json_encode(["message" => "Student updated successfully"]);
-}
-else if($_GET["action"] == "delete_student")
-{
-    $stmt = $db->prepare("delete from students where id = :id");
-    $stmt->bindParam("id", $_POST["id"]);
-    $stmt->execute();
-    echo json_encode(["message" => "Student deleted successfully"]);
+    case "get_students":
+        $stmt = $db->prepare("select * from students");
+        $stmt->execute();
+        $students = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $data = [];
+    
+        foreach($students as $student)
+        {
+            $stmt = $db->prepare("select courses.name, courses.id from student_courses inner join courses on courses.id = student_courses.course_id where student_id = " . $student["id"]);
+            $stmt->execute();
+            $courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $course_names = [];
+
+            foreach ($courses as $course) 
+            {
+                array_push($course_names, $course["name"]);
+            }
+
+            array_push($data, [
+                "id" => (int)$student["id"],
+                "name" => $student["name"],
+                "email" => $student["email"],
+                "phone" => (int)$student["phone"],
+                "gender" => $student["gender"],
+                "image_url" => $student["image_url"],
+                "address" => $student["address"],
+                "is_active" => $student["is_active"] == "1",
+                "courses" => $course_names
+            ]);
+        }
+
+        header("Content-Type: application/json");
+        echo json_encode($data);
+        break;
+    
+    case "create_student":
+        if(isset($_FILES["image"]))
+        {
+            $image_path = "uploads/" . bin2hex(random_bytes(16)) . "." . pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
+            move_uploaded_file($_FILES["image"]["tmp_name"], $image_path);
+        }
+        else 
+        {
+            $image_path = Null;
+        }
+    
+        $stmt = $db->prepare("insert into students (name, email, phone, image_url, is_active, gender, address) values (:name, :email, :phone, :image_url, :is_active, :gender, :address)");
+        $stmt->bindParam("name", $_POST["name"]);
+        $stmt->bindParam("email", $_POST["email"]);
+        $stmt->bindParam("phone", $_POST["phone"]);
+        $stmt->bindValue("is_active", $_POST["is_active"] == "true");
+        $stmt->bindParam("image_url", $image_path);
+        $stmt->bindParam("gender", $_POST["gender"]);
+        $stmt->bindParam("address", $_POST["address"]);
+        $stmt->execute();
+        $student_id = $db->lastInsertId();
+    
+        foreach ($_POST["course_ids"] as $course_id) 
+        {
+            $stmt = $db->prepare("insert into student_courses (student_id, course_id) values (:student_id, :course_id)");
+            $stmt->bindParam("student_id", $student_id);
+            $stmt->bindParam("course_id", $course_id);
+            $stmt->execute();
+        }
+
+        header("Content-Type: application/json");
+        echo json_encode(["message" => "Student created successfully"]);
+        break;
+
+    case "update_student":
+        file_put_contents("demo.json", json_encode($_POST));
+        $stmt = $db->prepare("select * from students where id = :id");
+        $stmt->bindParam("id", $_POST["id"]);
+        $stmt->execute();
+        $student = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if(isset($_FILES["image"]))
+        {
+            $image_path = "uploads/" . bin2hex(random_bytes(16)) . "." . pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
+            move_uploaded_file($_FILES["image"]["tmp_name"], $image_path);
+        }
+        else 
+        {
+            $image_path = $student["image_url"];
+        }
+
+        $stmt = $db->prepare("update students set name = :name, email = :email, phone = :phone, is_active = :is_active, gender = :gender, address = :address, image_url = :image_url where id = :id");
+        $stmt->bindParam("id", $_POST["id"]);
+        $stmt->bindParam("name", $_POST["name"]);
+        $stmt->bindParam("email", $_POST["email"]);
+        $stmt->bindParam("phone", $_POST["phone"]);
+        $stmt->bindValue("is_active", $_POST["is_active"] == "true");
+        $stmt->bindParam("image_url", $image_path);
+        $stmt->bindParam("gender", $_POST["gender"]);
+        $stmt->bindParam("address", $_POST["address"]);
+        $stmt->execute();
+
+        $stmt = $db->prepare("delete from student_courses where student_id = :student_id");
+        $stmt->bindParam("student_id", $_POST["id"]);
+        $stmt->execute();
+
+        foreach ($_POST["course_ids"] as $course_id) 
+        {
+            $stmt = $db->prepare("insert into student_courses (student_id, course_id) values (:student_id, :course_id)");
+            $stmt->bindParam("student_id", $_POST["id"]);
+            $stmt->bindParam("course_id", $course_id);
+            $stmt->execute();
+        }
+
+        header("Content-Type: application/json");
+        echo json_encode(["message" => "Student updated successfully"]);
+        break;
+
+    case "delete_student":
+        $stmt = $db->prepare("delete from students where id = :id");
+        $stmt->bindParam("id", $_POST["id"]);
+        $stmt->execute();
+
+        header("Content-Type: application/json");
+        echo json_encode(["message" => "Student deleted successfully"]);
+        break;
+
+    case "get_student":
+        $data = [];
+
+        $stmt = $db->prepare("select * from students where id = :id limit 1");
+        $stmt->bindParam("id", $_GET["id"]);
+        $stmt->execute();
+        $student = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $stmt = $db->prepare("select courses.id from student_courses inner join courses on courses.id = student_courses.course_id where student_courses.student_id = ". $student["id"]);
+        $stmt->execute();
+        $courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $course_ids = [];
+
+        foreach ($courses as $course) 
+        {
+            array_push($course_ids, $course["id"]);
+        }
+
+        header("Content-Type: application/json");
+        echo json_encode([
+            "id" => (int)$student["id"],
+            "name" => $student["name"],
+            "email" => $student["email"],
+            "phone" => (int)$student["phone"],
+            "gender" => $student["gender"],
+            "image_url" => $student["image_url"],
+            "address" => $student["address"],
+            "is_active" => $student["is_active"] == "1",
+            "course_ids" => $course_ids
+        ]);
+        break;
+
+    case "get_courses":
+        $stmt = $db->prepare("select * from courses");
+        $stmt->execute();
+        header("Content-Type: application/json");
+        echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+        break;
+
+    default:
+        http_response_code(404);
+        header("Content-Type: application/json");
+        echo json_encode(["error" => "Action not found!"]);
 }
--- angular/script.js
+++ angular/script.js
@@ -10,10 +10,10 @@ app.component("navbar", {
         toggleTheme: "&"
     },
     templateUrl: 'navbar.html',
-    controller: function($scope) {
-        this.send = function(data) {
+    controller: function ($scope) {
+        this.send = function (data) {
             this.title = 5
-            this.showAlert({message:'This data will be send to the parent component' + data})
+            this.showAlert({ message: 'This data will be send to the parent component' + data })
         }
     }
 })
@@ -21,117 +21,112 @@ app.component("navbar", {
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
-          templateUrl: "home.html",
-          title: "Home",
-          controller: "HomeController"
+            templateUrl: "home.html",
+            controller: "HomeController"
         })
         .when("/create", {
-          templateUrl: "create-student.html",
-          controller: "CreateStudentController"
+            templateUrl: "create-student.html",
+            controller: "CreateStudentController"
         })
         .when("/edit", {
-          templateUrl: "edit-student.html",
-          controller: "EditStudentController"
+            templateUrl: "edit-student.html",
+            controller: "EditStudentController"
         })
         .when("/contact", {
-          templateUrl: "contact.html",
-          controller: "ContactController"
+            templateUrl: "contact.html",
+            controller: "ContactController"
         })
         .otherwise({ redirectTo: "/" })
 })
 
-app.service("MathService", function() {
-    this.add = function(a, b) { return a + b };
+app.service("MathService", function () {
+    this.add = function (a, b) { return a + b };
 
-    this.subtract = function(a, b) { return a - b };
+    this.subtract = function (a, b) { return a - b };
 
-    this.multiply = function(a, b) { return a * b };
+    this.multiply = function (a, b) { return a * b };
 
-    this.divide = function(a, b) { return a / b };
+    this.divide = function (a, b) { return a / b };
 })
 
 app.controller("IndexController", function ($scope, $location) {
-    $scope.theme = "dark"
+    $scope.theme = "Dark"
 
-    $scope.toggleTheme = function() {
-        $scope.theme = $scope.theme == "dark" ? "light" : "dark"
+    $scope.toggleTheme = function () {
+        $scope.theme = $scope.theme == "Dark" ? "Light" : "Dark"
     }
-
-    $scope.isActive = function(destination) {
-      return destination == $location.path()
-    }
-
-    $scope.title = 10
-
-    $scope.$watch("title", function(oldValue, newValue){
-      console.log(oldValue, newValue);
-    })
-
-    $scope.show = function(arg){
-      console.log(arg);
-    }
-
-    $scope.message = "This is navbar of the application"
 })
 
 app.controller("HomeController", function ($scope, $http) {
     $scope.students = []
-    $scope.isLoading = false
 
-    $scope.deleteStudent = function(){
-        if(!confirm("Are you sure you want to delete " + this.student.name)) return
+    $scope.deleteStudent = function () {
+        if (!confirm("Are you sure you want to delete " + this.student.name)) return
 
         const payload = new FormData()
         payload.append("id", this.student.id)
 
         fetch("/ajax.php?action=delete_student", {
-          method: "post",
-          body: payload
+            method: "post",
+            body: payload
         })
         .then(response => {
             $scope.fetchStudents()
         })
     }
 
-    $scope.fetchStudents = function() {
-        $http({
-          url: "/ajax.php?action=get_students",
-          method: "get"
-        })
-        .then(response => {
-            $scope.students = response.data
-            console.log(response.data);
+    $scope.fetchStudents = function () {
+        fetch("/ajax.php?action=get_students")
+        .then(async response => {
+            $scope.students = await response.json()
+            $scope.$apply()
         })
     }
 
     $scope.fetchStudents()
 })
 
-app.directive('fileModel', ['$parse', function ($parse) {
+app.directive("checkModel", function () {
     return {
-       restrict: 'A',
-       link: function(scope, element, attrs) {
-        console.log(attrs);
-          var model = $parse(attrs.fileModel);
-          var modelSetter = model.assign;
+        restrict: "A",
+        link: function (scope, element, attrs) {
+            element[0].checked = scope.student.course_ids.find(course => course == attrs.value)
 
-          scope.reset = function() {
+            element.bind("change", function (event) {
+                if (event.target.checked) {
+                    scope.student.course_ids.push(Number(attrs.value))
+                } else {
+                    scope.student.course_ids = scope.student.course_ids.filter(id => id != Number(attrs.value))
+                }
+            })
+        }
+    }
+})
+
+app.directive("fileModel", ["$parse", function ($parse) {
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs) {
+            console.log(attrs);
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            scope.resetImage = function () {
                 element[0].value = ""
-                // console.log(document.querySelector("input[type=file]"));
-                // console.log(element[0]);
                 scope.student.image = ""
-                document.querySelector("input[type=file]").value = ""
-                console.log("call");
-          }
-          
-        element.bind('change', function() {
-             scope.$apply(function() {
-                modelSetter(scope, element[0].files[0]);
-             });
-          });
-       }
+                scope.student.image_url = ""
+            }
+
+            element.bind("change", function () {
+                scope.$apply(function () {
+                    modelSetter(scope, element[0].files[0]);
+                    scope.student.image_url = URL.createObjectURL(element[0].files[0])
+                });
+            });
+        }
     };
- }]);
+}]);
+
 
 app.controller("CreateStudentController", function ($scope, $location) {
     $scope.student = {
@@ -139,79 +134,63 @@ app.controller("CreateStudentController", function ($scope, $location) {
         email: "",
         phone: "",
         image: "",
-        active: false,
-        courses: [
-            { name: "PHP", value: true },
-            { name: "Java", value: true },
-            { name: "Javascript", value: false },
-        ]
-    }
-    $scope.isLoading = false 
-
-    $scope.$watch('student.courses[0].value', function(newValue, oldValue) {
-        console.log(newValue, oldValue, " Check box changed");
-    })
-
-    $scope.change = function(oldValue, newValue) {
-        console.log("Old value", oldValue);
-        console.log("New value", newValue);
+        image_url: "",
+        is_active: false,
+        course_ids: [],
+        gender: "",
+        address: ""
     }
 
-    $scope.createStudent = async function(){
+    $scope.courses = []
+
+    $scope.createStudent = async function () {
         const payload = new FormData()
         Object.keys($scope.student).forEach(key => {
-            if(key == "courses") {
+            if (key == "course_ids") {
                 $scope.student[key].forEach((course, index) => {
-                    console.log(`${key}[${index}][name]`);
-                    payload.append(`${key}[${index}][name]`, course.name)
-                    payload.append(`${key}[${index}][value]`, course.value)
+                    payload.append(`${key}[${index}]`, course)
                 })
             } else {
                 payload.append(key, $scope.student[key])
             }
         })
 
-        const response = await fetch("/ajax.php?action=create_student", {
+        await fetch("/ajax.php?action=create_student", {
             method: "post",
             body: payload
         })
 
-        console.log(await response.text());
-
-        $scope.student = {
-            name: "",
-            email: "",
-            phone: ""
-        }
-
-        $location.path('/')
+        $location.path("/")
         $scope.$apply()
     }
-})
 
+    fetch("/ajax.php?action=get_courses")
+        .then(async response => {
+            $scope.courses = await response.json()
+            $scope.$apply()
+        })
+})
 
 
 app.controller("EditStudentController", function ($scope, $location) {
     $scope.student = {
-        id: $location.search().id,
-        name: "",
-        email: "",
-        phone: ""
+        id: $location.search().id
     }
-    $scope.isLoading = true 
-    
-    fetch("/ajax.php?action=get_student&&id=" + $location.search().id)
-    .then(async response => {
-        $scope.student = await response.json()
-        $scope.$apply();
-    })
 
-    $scope.updateStudent = async function(){
+    $scope.courses = []
+
+    $scope.updateStudent = async function () {
         const payload = new FormData()
         Object.keys($scope.student).forEach(key => {
-            payload.append(key, $scope.student[key])
+            if (key == "course_ids") {
+                $scope.student[key].forEach((course, index) => {
+                    payload.append(`${key}[${index}]`, course)
+                })
+            } else {
+                payload.append(key, $scope.student[key])
+            }
         })
-    
+
         await fetch("/ajax.php?action=update_student", {
             method: "post",
             body: payload
@@ -220,22 +199,40 @@ app.controller("EditStudentController", function ($scope, $location) {
         $location.path('/')
         $scope.$apply()
     }
+
+    fetch("/ajax.php?action=get_student&&id=" + $location.search().id)
+        .then(async response => {
+            const data = await response.json()
+
+            $scope.student = {
+                ...data,
+                image: ""
+            }
+
+            $scope.$apply()
+        })
+
+    fetch("/ajax.php?action=get_courses")
+        .then(async response => {
+            $scope.courses = await response.json()
+            $scope.$apply();
+        })
 })
 
 app.controller('MainController', function ($scope) {
-    $scope.$on("FirstControllerEvent", function(event, data) {
-      $scope.$broadcast("MainControllerBoardCast", data)
+    $scope.$on("FirstControllerEvent", function (event, data) {
+        $scope.$broadcast("MainControllerBoardCast", data)
     })
 })
 
 app.controller('FirstController', function ($scope) {
-    $scope.sendEvent = function() {
-      $scope.$emit("FirstControllerEvent", "This is data")
+    $scope.sendEvent = function () {
+        $scope.$emit("FirstControllerEvent", "This is data")
     }
 })
 
 app.controller('SecondController', function ($scope) {
-    $scope.$on("MainControllerBoardCast", function(event, data) {
+    $scope.$on("MainControllerBoardCast", function (event, data) {
         $scope.data = data
     })
 })
